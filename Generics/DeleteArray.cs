d4426c2f45e5e2ef6ed2008189b39763607ef7ca
Akshay4955 <akshayshedge11@gmail.com>
2023-05-31 19:05:32 +0530

    Added: Code to delete element from array

---
+++ Generics/DeleteArray.cs
@@ -0,0 +1,51 @@
+﻿namespace Generics;
+
+public class DeleteArray
+{
+    public void IntDeleteMethod(int[] intArray, int position)
+    {
+        int index = position;
+        for (int i = index; i < intArray.Length - 1; i++) 
+        {
+            intArray[i] = intArray[i + 1];
+        }
+
+        Console.WriteLine("Int array after deletion");
+        for (int i = 0; i < intArray.Length - 1; i++)
+        {
+            Console.Write(intArray[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void DoubleDeleteMethod(double[] doubleArray, int position)
+    {
+        int index = position;
+        for (int i = index; i < doubleArray.Length - 1; i++)
+        {
+            doubleArray[i] = doubleArray[i + 1];
+        }
+
+        Console.WriteLine("double Array after deletion");
+        for (int i = 0; i < doubleArray.Length - 1; i++)
+        {
+            Console.Write(doubleArray[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void CharacterDeleteMethod(char[] charArray, int position)
+    {
+        int index = position;
+        for (int i = index; i < charArray.Length - 1; i++)
+        {
+            charArray[i] = charArray[i + 1];
+        }
+
+        Console.WriteLine("char Array after deletion");
+        for (int i = 0; i < charArray.Length - 1; i++)
+        {
+            Console.Write(charArray[i] + " ");
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Generics
+﻿namespace Generics;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to generics problems");
-        }
+        Console.WriteLine("Welcome to generics problems");
+        int[] intArray = new int[] {12, 18, 25 };
+        double[] doubleArray = new double[] { 12.76, 22.76, 32.67 };
+        char[] charArray = new char[] { 'a', 'g', 'm' };
+        DeleteArray deleteArray = new DeleteArray();
+        deleteArray.IntDeleteMethod(intArray, 1);
+        deleteArray.DoubleDeleteMethod(doubleArray, 0);
+        deleteArray.CharacterDeleteMethod(charArray, 2);
     }
 }
