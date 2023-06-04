a75358929a58451d36b7802718ce23b0f05838ea
Akshay4955 <akshayshedge11@gmail.com>
2023-06-04 12:27:52 +0530

    Added: Code for generic class to find minimum

---
+++ Generics/GenMinimum.cs
@@ -0,0 +1,17 @@
+﻿namespace Generics;
+
+public class GenMinimum<T> where T : IComparable
+{
+    public void FindMinimum(T[] array)
+    {
+        T minimum;
+        if (array[0].CompareTo(array[1]) < 0 && array[0].CompareTo(array[2]) < 0)
+            minimum = array[0];
+        else if (array[1].CompareTo(array[0]) < 0 && array[1].CompareTo(array[2]) < 0)
+            minimum = array[1];
+        else
+            minimum = array[2];
+
+        Console.WriteLine($"Minimum from given array is : {minimum}");
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,12 +4,13 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to generics problems");
+        Console.WriteLine("Welcome to generics problems.....!!!!");
         int[] intArray = new int[] { 12, 18, 25 };
         double[] doubleArray = new double[] { 12.76, 22.76, 32.67 };
         float[] floatArray = new float[] { 13.65f, 17.53f, 11.76f };
         char[] charArray = new char[] { 'a', 'g', 'm' };
         string[] stringArray = new string[] { "Akshay", "Rahul", "Rupesh" };
+
         DeleteArray<int> deleteArray = new DeleteArray<int>();
         deleteArray.DeleteMethod(intArray, 1);
         DeleteArray<double> doubleDeleteArray = new DeleteArray<double>();
@@ -17,13 +18,31 @@ internal class Program
         DeleteArray<char> charDeleteArray = new DeleteArray<char>();
         charDeleteArray.DeleteMethod(charArray, 2);
 
+        Console.WriteLine();
+        Console.WriteLine("Minimum with normal method :");
+        Console.WriteLine("-------------------------------------------------------");
         MinimumArray minimumArray = new MinimumArray();
         minimumArray.FindMinimumInt(intArray);
         minimumArray.FindMinimumFloat(floatArray);
         minimumArray.FindMinimumString(stringArray);
 
+        Console.WriteLine();
+        Console.WriteLine("Minimum with Generic method :");
+        Console.WriteLine("-------------------------------------------------------");
         minimumArray.FindMinimum(intArray);
         minimumArray.FindMinimum(floatArray);
         minimumArray.FindMinimum(stringArray);
+
+        Console.WriteLine();
+        Console.WriteLine("Minimum with Generic class :");
+        Console.WriteLine("-------------------------------------------------------");
+        GenMinimum<int> genMinimum = new GenMinimum<int>();
+        genMinimum.FindMinimum(intArray);
+
+        GenMinimum<float> genMinimumFloat = new GenMinimum<float>();
+        genMinimumFloat.FindMinimum(floatArray);
+
+        GenMinimum<string> genMinimumString = new GenMinimum<string>();
+        genMinimumString.FindMinimum(stringArray);
     }
 }
