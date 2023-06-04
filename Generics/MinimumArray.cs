683d3aaedb3ad893677e7d3321d827f18ce64c5a
Akshay4955 <akshayshedge11@gmail.com>
2023-06-04 11:37:54 +0530

    Added: Code to find minimum from float array

--- Generics/MinimumArray.cs
+++ Generics/MinimumArray.cs
@@ -2,7 +2,7 @@
 
 public class MinimumArray
 {
-    public void FindMinimum(int[] intArray)
+    public void FindMinimumInt(int[] intArray)
     {
         int minimum = 0;
         if (intArray[0].CompareTo(intArray[1]) < 0 && intArray[0].CompareTo(intArray[2]) < 0)
@@ -13,5 +13,18 @@ public class MinimumArray
             minimum = intArray[2];
 
         Console.WriteLine($"Minimum int number from given three integer array is : {minimum}");
+    }
+
+    public void FindMinimumFloat(float[] floatArray) 
+    {
+        float minimum = 0;
+        if (floatArray[0].CompareTo(floatArray[1]) < 0 && floatArray[0].CompareTo(floatArray[2]) < 0)
+            minimum = floatArray[0];
+        else if (floatArray[1].CompareTo(floatArray[0]) < 0 && floatArray[1].CompareTo(floatArray[2]) < 0)
+            minimum = floatArray[1];
+        else
+            minimum = floatArray[2];
+
+        Console.WriteLine($"Minimum float number from given three float array is : {minimum}");
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,8 +5,9 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to generics problems");
-        int[] intArray = new int[] {12, 18, 25 };
+        int[] intArray = new int[] { 12, 18, 25 };
         double[] doubleArray = new double[] { 12.76, 22.76, 32.67 };
+        float[] floatArray = new float[] { 13.65f, 17.53f, 11.76f };
         char[] charArray = new char[] { 'a', 'g', 'm' };
         DeleteArray<int> deleteArray = new DeleteArray<int>();
         deleteArray.DeleteMethod(intArray, 1);
@@ -16,6 +17,7 @@ internal class Program
         charDeleteArray.DeleteMethod(charArray, 2);
 
         MinimumArray minimumArray = new MinimumArray();
-        minimumArray.FindMinimum(intArray);
+        minimumArray.FindMinimumInt(intArray);
+        minimumArray.FindMinimumFloat(floatArray);
     }
 }
